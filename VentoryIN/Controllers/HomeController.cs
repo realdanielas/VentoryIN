785f5be5b35d733c167088c1e89a7f0eca112899
Daniela Saavedra <ana.saavedra@catolica.edu.sv>
2024-10-05 19:19:26 -0600

    Creación de interfaz "Clasificación de Productos".

--- VentoryIN/Controllers/HomeController.cs
+++ VentoryIN/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace VentoryIN.Controllers
             return View();
         }
 
+        public IActionResult ClasificacionProductos()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
