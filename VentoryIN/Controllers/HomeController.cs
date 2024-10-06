90a95a5b197f996c26d0cacd60650b56aa96749c
Daniela Saavedra <ana.saavedra@catolica.edu.sv>
2024-10-05 20:46:40 -0600

    Creación de interfaz "Entradas".

--- VentoryIN/Controllers/HomeController.cs
+++ VentoryIN/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace VentoryIN.Controllers
             return View();
         }
 
+        public IActionResult Salida()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
